26dd1c38977d5a2b1e2d113a84251fad8d15f02b
juanedcabrera <juanedcabrera@gmail.com>
2023-06-02 13:13:33 -0400

    Introduction phase 1 complete

--- components/Introduction/page.jsx
+++ components/Introduction/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Introduction = () => {
   return (
     <section>
-      <p>Long-form message goes here...</p>
+      <p>"Juan Cabrera, a resilient and determined individual, transitioned from being an accomplished Senior Talent Acquisition Partner to embark on a new path as a Software Engineer. Throughout his career, Juan consistently demonstrated excellence, delivering outstanding results and taking on monumental projects that earned him accolades. When faced with adversity, Juan harnessed his unwavering work ethic, dedicating over 1200 hours in just 3 months, working an average of 15 hours a day, to kickstart his software engineering journey. With his deep drive for success and proven ability to tackle challenges head-on, Juan brings a unique blend of expertise in business challenges and software engineering, ensuring valuable insights and innovative solutions in every endeavor."</p>
     </section>
   );
 };
