82995377d677f57eea1dd71df25ebf973e8bcf8b
juanedcabrera <juanedcabrera@gmail.com>
2023-06-02 16:45:01 -0400

    Grids achieved

--- components/NavBlock/page.jsx
+++ components/NavBlock/page.jsx
@@ -5,9 +5,9 @@ const NavBlock = () => {
   return (
     <footer>
       <div className={`${styles.navBlock}`}>
-        <a href="/blog" className={`${styles.navLink}`}>Blog</a>
-        <a href="/readlist" className={`${styles.navLink}`}>Readlist</a>
-        <a href="/projects" className={`${styles.navLink}`}>Projects</a>
+        <a href="/blog" className={`${styles.navLink1}`}>Blog</a>
+        <a href="/readlist" className={`${styles.navLink2}`}>Readlist</a>
+        <a href="/projects" className={`${styles.navLink3}`}>Projects</a>
       </div>
     </footer>
   );
