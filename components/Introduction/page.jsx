b3f2d078bfe8453573485b7ad23621963a4b4ae3
juanedcabrera <juanedcabrera@gmail.com>
2023-06-02 18:10:03 -0400

    Introduction modified and colors updated

--- components/Introduction/page.jsx
+++ components/Introduction/page.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './page.module.css';
 
 const Introduction = () => {
   return (
-    <section>
-      <p>"Juan Cabrera, a resilient and determined individual, transitioned from being an accomplished Senior Talent Acquisition Partner to embark on a new path as a Software Engineer. Throughout his career, Juan consistently demonstrated excellence, delivering outstanding results and taking on monumental projects that earned him accolades. When faced with adversity, Juan harnessed his unwavering work ethic, dedicating over 1200 hours in just 3 months, working an average of 15 hours a day, to kickstart his software engineering journey. With his deep drive for success and proven ability to tackle challenges head-on, Juan brings a unique blend of expertise in business challenges and software engineering, ensuring valuable insights and innovative solutions in every endeavor."</p>
+    <section className={`${styles.introduction}`}>
+      <h1><em className={styles.italicized}>Juan Cabrera</em>, a resilient and determined <em className={styles.italicized}>dad</em> of 2, transitioned from being an <em className={styles.italicized}>award-winning Senior Talent Acquisition Partner</em> to embark on a new path as a Software Engineer. He harnessed his unwavering work ethic, dedicating over <em className={styles.italicized}>1200 hours in just 3 months, working an average of 15 hours a day,</em> to kickstart his software engineering journey. Check out the page to see more.</h1>
     </section>
   );
 };
