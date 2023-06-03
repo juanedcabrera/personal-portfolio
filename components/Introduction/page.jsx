6a8e89731c6ed19ad31e395f1d9971f277db4cf0
juanedcabrera <juanedcabrera@gmail.com>
2023-06-02 22:40:36 -0400

    Introduction typography and actions astarted

--- components/Introduction/page.jsx
+++ components/Introduction/page.jsx
@@ -1,10 +1,65 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import styles from './page.module.css';
+import Image from 'next/image';
 
 const Introduction = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleModalOpen = () => {
+    setShowModal(true);
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
   return (
     <section className={`${styles.introduction}`}>
-      <h1><em className={styles.italicized}>Juan Cabrera</em>, a resilient and determined <em className={styles.italicized}>dad</em> of 2, transitioned from being an <em className={styles.italicized}>award-winning Senior Talent Acquisition Partner</em> to embark on a new path as a Software Engineer. He harnessed his unwavering work ethic, dedicating over <em className={styles.italicized}>1200 hours in just 3 months, working an average of 15 hours a day,</em> to kickstart his software engineering journey. Check out the page to see more.</h1>
+      <h1>
+        <a
+          href="https://www.linkedin.com/in/juanedcabrera"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <em className={`${styles.italicized} ${styles.underline}`}>
+            Juan E. Cabrera
+          </em>
+        </a>
+        , a resilient and determined{' '}
+        <em
+          className={`${styles.italicized} ${styles.hoverImage}`}
+          onClick={handleModalOpen}
+        >
+          dad
+        </em>{' '}
+        of 2, transitioned from being an{' '}
+        <em className={styles.italicized}>award-winning </em>Senior Talent
+        Acquisition Partner to embark on a new path as a{' '}
+        <a
+          href="https://www.github.com/juanedcabrera"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <em className={`${styles.italicized} ${styles.underline}`}>
+            Software Engineer
+          </em>
+        </a>
+        . To kickstart his software engineering journey, he worked an average of
+        15 hours a day to complete his General Assembly Software Engineering
+        Immersive bootcamp in 3 months. Check out his work.
+      </h1>
+
+      {showModal && (
+        <div className={styles.modal} onClick={handleModalClose}>
+          <Image
+            src="/../public/uploads/dad.jpeg"
+            alt="Dad"
+            height="250"
+            width="500"
+          />
+        </div>
+      )}
     </section>
   );
 };
