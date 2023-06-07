93a9acdc6d8a49b815391db14f1cec0d4cc7e0e5
juanedcabrera <juanedcabrera@gmail.com>
2023-06-06 22:41:39 -0400

    Readlist layout and page

--- app/(branches)/readlist/layout.jsx
+++ app/(branches)/readlist/layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../../styles/globals.css'
-import Header from '../../../components/Header/page.jsx';
+
 
 export const metadata = {
   title: 'Juan Cabrera, Software Engineer',
